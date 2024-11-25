621d4e2b7a4bc2d6fc09b5561287abe6fc3127c4
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2024-11-25 23:27:04 +0100

    fixed auth

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  ImageBackground,
-} from "react-native";
-import axios from "axios";
-import { useNavigation } from "@react-navigation/native";
-import { API_BASE_URL } from "../services/apiConfig";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Modal, Alert, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { API_BASE_URL } from '../services/apiConfig';
 
-export default function LoginScreen() {
-
+function Login() {
   const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [showPassword, setShowPassword] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const login = async () => {
     try {
@@ -26,17 +20,26 @@ export default function LoginScreen() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          email,
+          user:{
+            email,
           password,
+          }
         }),
       });
 
       if (response.ok) {
         const data = await response.json();
-        const token = data.access_token;
-        console.log('Token:', token);
-        await AsyncStorage.setItem('authToken', token);
-        navigation.navigate('DrawerNavigation');
+        const token = data.token;
+        const userRole = data.type;
+        if (userRole === "Doctor") {
+          navigation.navigate("Doctor");
+          await AsyncStorage.setItem('authToken', token);
+        } else if (userRole === "Patient") {
+          navigation.navigate("Patient");
+          await AsyncStorage.setItem('authToken', token);
+        } else {
+          alert("Invalid role");
+        }
       } else {
         console.error('Login failed');
       }
@@ -45,86 +48,146 @@ export default function LoginScreen() {
     }
   };
 
+  const goToRegister = () => {
+    navigation.navigate('Register');
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const requestNewPassword = async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/user/newpassword`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        Alert.alert('Success', data.message);
+        toggleModal();
+      } else {
+        const errorData = await response.json();
+        Alert.alert('Error', errorData.message);
+      }
+    } catch (error) {
+      console.error('Error requesting new password:', error);
+      Alert.alert('Error', 'An error occurred while requesting a new password. Please try again later.');
+    }
+  };
+
   return (
-    <ImageBackground
-      source={require("../assets/images/image.png")}
-      style={styles.container}
-    >
-      <View style={styles.formContainer}>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View  style={styles.formContainer}>
         <Text style={styles.title}>Login</Text>
         <TextInput
+          style={styles.input}
           placeholder="Email"
-          style={styles.input}
-          placeholderTextColor="#7ABFCF"
+          placeholderTextColor="#666"
           value={email}
-          onChangeText={setEmail}
+          onChangeText={(text) => setEmail(text)}
         />
-        <TextInput
-          placeholder="Password"
-          secureTextEntry
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          placeholderTextColor="#7ABFCF"
-        />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText} onPress={login}>
-            Login
-          </Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="Password"
+            placeholderTextColor="#666"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <TouchableOpacity onPress={togglePasswordVisibility}>
+            <MaterialCommunityIcons name={showPassword ? 'eye' : 'eye-off'} size={22} color="#333" />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity onPress={login} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={toggleModal}>
           <Text style={styles.link}>Forgot Password?</Text>
         </TouchableOpacity>
       </View>
-    </ImageBackground>
+      <Modal visible={showModal} animationType="slide" transparent>
+        <KeyboardAvoidingView style={styles.container}>
+          <View style={styles.formContainer}>
+            <Text style={styles.title}>Reset Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Your Email"
+              placeholderTextColor="#666"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+            <TouchableOpacity onPress={requestNewPassword} style={styles.button}>
+              <Text style={styles.buttonText}>Send</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={toggleModal}>
+              <Text style={styles.link}>Back</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    alignItems: "center",
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   formContainer: {
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
-    width: "90%",
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    width: '90%',
     borderRadius: 20,
     padding: 20,
   },
   title: {
     fontSize: 28,
-    fontWeight: "bold",
-    color: "#0F9BAE",
-    textAlign: "center",
+    fontWeight: 'bold',
+    color: '#0F9BAE',
+    textAlign: 'center',
     marginBottom: 20,
   },
   input: {
     borderWidth: 1,
-    borderColor: "#BCE4EA",
+    borderColor: '#BCE4EA',
     borderRadius: 10,
     padding: 15,
     marginBottom: 15,
-    backgroundColor: "#F3FAFB",
-    color: "#0F9BAE",
+    backgroundColor: '#F3FAFB',
+    color: '#0F9BAE',
   },
   button: {
-    backgroundColor: "#0F9BAE",
+    backgroundColor: '#0F9BAE',
     padding: 15,
     borderRadius: 10,
-    alignItems: "center",
+    alignItems: 'center',
     marginBottom: 10,
   },
   buttonText: {
-    color: "#FFF",
-    fontWeight: "bold",
+    color: '#FFF',
+    fontWeight: 'bold',
     fontSize: 16,
   },
   link: {
-    color: "#0F9BAE",
-    textAlign: "center",
+    color: '#0F9BAE',
+    textAlign: 'center',
     marginTop: 10,
-    textDecorationLine: "underline",
+    textDecorationLine: 'underline',
   },
 });
+
+export default Login;
